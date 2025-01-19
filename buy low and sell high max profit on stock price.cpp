844f69e9790e58bd87668806811bbb0e7a5a6ba4
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-01-19 23:56:56 +0530

    Create buy low and sell high max profit on stock price.cpp

---
+++ buy low and sell high max profit on stock price.cpp
@@ -0,0 +1,43 @@
+// Online C++ compiler to run C++ program online
+
+// You are given an array prices where prices[i] is the price of a given stock on
+// the day.
+// You want to maximize your prot by choosing a single day to buy one stock and
+// choosing a dierent
+// day in the future to sell that stock.
+// Return the maximum prot you can achieve from this transacon. If you
+// cannot achieve any
+// prot, return 0.
+// Input-1 : prices = [7,1,5,3,6,4]
+// Output-1 : 5 
+// Explanaon: Buy on day 2 (price = 1) and sell on day 5 (price = 6), prot = 6-1 =
+// 5.
+// Input-2 : prices = [7,6,4,3,1]
+// Output-2 : 0
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int array[6] = {7,1,5,3,6,4};
+    // int array[5] = {7,6,4,3,1};
+    
+    int buy, sell, max_profit=0;
+    for(int i=0; i<5; i++)
+    {
+        for(int j=i+1; j<5; j++)
+        {
+            if(array[j]-array[i]>max_profit)
+            {
+                max_profit=array[j]-array[i];
+                
+                buy=array[i];
+                sell=array[j];
+            }
+        }
+    }
+    cout<<"Buy stock at "<<buy<<" and sell at "<<sell<<". Profit : "<<max_profit;
+    return 0;
+}
