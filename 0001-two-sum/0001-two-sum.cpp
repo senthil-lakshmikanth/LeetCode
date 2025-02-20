415fb0ea240cb7355e4cedf4d7bce4c457a4f403
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-02-20 23:07:15 +0530

    Time: 2 ms (78.89%), Space: 16 MB (7.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,18 +8,14 @@ public:
         for(int i = 0; i < nums.size(); i++)
             hashed_num[nums[i]] = i;
 
-        vector <int> output;
-
         for(int i = 0; i < nums.size(); i++)
         {
             if(hashed_num.find(target - nums[i]) != hashed_num.end() and i != hashed_num[target - nums[i]])
             {
-                output.push_back(i);
-                output.push_back(hashed_num[target - nums[i]]);
-                break;
+                return { i, hashed_num[target - nums[i]] };
             } 
         }
 
-        return output;
+        return nums;
     }
 };
