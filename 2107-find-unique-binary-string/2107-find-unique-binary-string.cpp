f887bf247f006fd8467edc8f5c970c6262fab5b5
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-02-20 18:44:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (99.65%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -4,28 +4,16 @@ public:
     string findDifferentBinaryString(vector<string>& nums) 
     {
         int num_size = nums.size();
+        string output = "";
 
-        unordered_set <string> nums_set (nums.begin(), nums.end());
-        
-        return sequence(nums_set, num_size, "");
-    }
-
-    string sequence(unordered_set <string>& nums_set, int num_size, string num)
-    {
-        if(num.size() == num_size)
+        for(int index = 0; index < num_size; index++)
         {
-            if(nums_set.find(num) == nums_set.end())
-                return num;
+            if(nums[index][index] == '0')
+                output += '1';
             else
-                return "";
+                output += '0'; 
         }
-        
-        for(auto binary : {'0', '1'})
-        {
-            string output = sequence(nums_set, num_size, num + binary);
-            if(!output.empty())
-                return output;
-        }
-        return "";
+
+        return output;
     }
 };
