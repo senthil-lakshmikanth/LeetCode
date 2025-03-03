caf07b8cebe22835bcf7849a13dd30a02626e956
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-03 18:31:01 +0530

    Time: 3 ms (38.64%), Space: 41.1 MB (42.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,12 +5,6 @@ public:
     {
         int size = nums.size();
 
-        vector <int> prefix (size, 1);
-        for(int i = 1; i < size; i++)
-        {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
-        }
-
         vector <int> postfix (size, 1);
         for(int i = size - 1 - 1; i >=0; i--)
         {
@@ -18,9 +12,11 @@ public:
         }
 
         vector <int> result (size);
+        int prefix = 1;
         for(int i = 0; i < size; i++)
         {
-            result[i] = prefix[i] * postfix[i];
+            result[i] = postfix[i] * prefix;
+            prefix = prefix * nums[i]; 
         }
 
         return result;
