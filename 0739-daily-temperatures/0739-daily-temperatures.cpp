c800700e1978a8ca5d3b9b097a774424b53cfff6
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-12 20:04:54 +0530

    Time: 44 ms (7.13%), Space: 111.8 MB (12.46%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) 
+    {
+        int n = temperatures.size();
+        vector <int> result (n, 0);
+
+        stack <pair <int, int>> stack; // pair : {temp, index}
+
+        for(int i = 0; i < n; i++)
+        {
+            if(stack.empty() || temperatures[i] < stack.top().first)
+            {
+                stack.push({temperatures[i], i});
+            }
+            else
+            {
+                while(!stack.empty() && temperatures[i] > stack.top().first)
+                {
+                    auto pair = stack.top();
+                    result[pair.second] = i - pair.second;
+                    stack.pop();
+                }
+                stack.push({temperatures[i], i});
+            }
+        }
+        return result;
+    }
+};
