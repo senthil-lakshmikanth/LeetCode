d42e3df770f069de813095c821533b1cc2c3bcbe
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-12 17:26:31 +0530

    Time: 0 ms (100%), Space: 21.4 MB (43.55%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -3,20 +3,43 @@ class Solution
 public:
     int maximumCount(vector<int>& nums) 
     {
-        int pos = 0;
-        int neg = 0;
-
         int n = nums.size();
 
-        int left = 0;
-        int right = n - 1;
-
-        while(left < n and nums[left] < 0) left++;
-        while(right >= 0 and nums[right] > 0) right--;
-
-        pos = n - (right + 1);
-        neg = left - 0;
-
+        int neg = findFirstNonNegative(nums) - 0; // ≥ 0
+        int pos = n - findFirstPositive(nums);  // > 0
+        
         return max(pos, neg);
+    }
+
+    int findFirstNonNegative(vector <int>& nums)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[mid] < 0)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+
+    int findFirstPositive(vector <int>& nums)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <=  right)
+        {
+            int mid = (left + right) / 2; 
+            if(nums[mid] <= 0)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
     }
 };
