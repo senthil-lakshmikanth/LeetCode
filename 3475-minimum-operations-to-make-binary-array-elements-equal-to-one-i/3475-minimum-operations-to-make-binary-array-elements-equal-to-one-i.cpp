b58c3c17557f74f80527adaf81c75fd39b270acb
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-19 17:13:12 +0530

    Time: 4 ms (81.56%), Space: 139.9 MB (93.66%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -7,22 +7,20 @@ public:
 
         int count = 0;
 
-        for(int i = 0; i < n - 2; i++)
+        for(int i = 0; i < n; i++)
         {
-            if(nums[i] == 0)
+            if(!nums[i]) // if(nums[i] == 0)
             {
-                nums[i]     = 1;
-                nums[i + 1] = 1 - nums[i + 1];
-                nums[i + 2] = 1 - nums[i + 2];
-
+                if(i + 1 >= n || i + 2 >= n) return -1;
+                else
+                {
+                    nums[i]      = 1;
+                    nums[i + 1] ^= 1;
+                    nums[i + 2] ^= 1;
+                }
                 count++;
             }
         }
-
-        for(int num : nums)
-            if(!num)
-                return -1;
-
         return count;    
     }
 };
