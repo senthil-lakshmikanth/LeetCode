4353697c142ce290f49a6367c99f93ac76d68cc3
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-25 11:54:51 +0530

    Time: 730 ms (7.22%), Space: 402.6 MB (5.25%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,39 @@
+class Solution 
+{
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) 
+    {
+        vector <vector <int>> x; // Interval (or) Length of each side along x-axis 
+        vector <vector <int>> y; // Interval (or) Length of each side along y-axis
+
+        for(auto coordinates : rectangles)
+        {
+            x.push_back({coordinates[0], coordinates[2]}); // {startx, endx}
+            y.push_back({coordinates[1], coordinates[3]}); // {starty, endy}
+        }
+
+        sort(x.begin(), x.end());
+        sort(y.begin(), y.end());
+
+        return max(isOverlapping(x), isOverlapping(y));
+    }
+
+    bool isOverlapping(vector <vector <int>> number_line)
+    {
+        int prevEnd = 0;
+        int count = 0;
+
+        for(auto edges : number_line)
+        {
+            int start = edges[0];
+            int end = edges[1];
+
+            if(prevEnd <= start) // It does not over lap.
+                count++;
+
+            prevEnd = max(prevEnd, end);
+        }
+
+        return count >= 3;
+    }
+};
