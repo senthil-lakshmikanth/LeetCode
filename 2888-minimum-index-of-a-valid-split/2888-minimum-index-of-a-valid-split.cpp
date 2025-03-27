30f3dd22c9026ec75d21a169484f1689e271d898
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-27 20:55:27 +0530

    Time: 4 ms (77.46%), Space: 89.7 MB (98.36%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -3,24 +3,40 @@ class Solution
 public:
     int minimumIndex(vector<int>& nums) 
     {
-        unordered_map <int, int> left, right;
-        int n = nums.size();
-
+        int majorityElement;
+        int count = 0;
         for(const int& num : nums)
-            right[num]++;
+        {
+            if(count == 0)
+                majorityElement = num;
+            
+            count += (majorityElement == num) ? 1 : -1;
+        }
+
+        int majorityLeftcount = 0;
+        int majorityRightcount = 0;
+        for(const int& num : nums)
+        {
+            if(num == majorityElement)
+                majorityRightcount++;
+        }
+
+
+        int n  = nums.size();
 
         for(int index = 0; index < n; index++)
         {
-            int num = nums[index];
+            if(nums[index] == majorityElement)
+            {
+                majorityRightcount--;
+                majorityLeftcount++;
 
-            right[num]--;
-            left[num]++;
+                int left_length = (index + 1); // (i + 1) - 0
+                int right_length = (n - 1) - index;
 
-            int left_length = (index + 1) - 0;
-            int right_length = (n - 1) - index;
-
-            if(left[num] > left_length / 2 && right[num] > right_length / 2)
-                return index;
+                if(majorityLeftcount > left_length / 2 && majorityRightcount > right_length / 2)
+                    return index;
+            }
         }
         return -1;
     }
