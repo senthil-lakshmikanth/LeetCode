e9e89313384c510ef899572346174a666ee304f7
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-03-31 19:46:23 +0530

    Time: 0 ms (100%), Space: 14 MB (79.05%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -12,12 +12,13 @@ public:
         {
             int mid = (left + right) / 2;
 
-            if(nums[mid] > nums[right])
-                left = mid + 1;
-            else
+            if(nums[mid] <= nums[right])
                 right = mid;
+            else
+                left = mid + 1;
         }
         
-        return nums[right];
+        return nums[right]; // nums[left] is also right, 
+                            // It exists from while loop, when 'left' and 'right' converge to the same index. while(left == right)
     }
 };
