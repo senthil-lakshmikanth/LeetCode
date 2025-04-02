5260cb17b65984ac535f26a52d9e10108dc943b9
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-04-02 13:51:24 +0530

    Time: 4 ms (63.64%), Space: 98.6 MB (56.01%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -10,16 +10,15 @@ public:
         vector <int> maxLeft (n);
         vector <int> maxRight (n);
 
-        maxLeft[0] = nums[0];
-        for(int i = 1; i < n; i++)
-            maxLeft[i] = max(nums[i], maxLeft[i - 1]);
-        
-        maxRight[n - 1] = nums[n - 1];
-        for(int k = n - 2; k >= 0; k--)
-            maxRight[k] = max(nums[k], maxRight[k + 1]);
+        for(int i = 1; i < n - 1; i++)
+        {
+            maxLeft[i] = max(nums[i - 1], maxLeft[i - 1]);
+            maxRight[n - 1 - i] = max(nums[n - i], maxRight[n - i]);
+        }
+
 
         for(int j = 1; j < n - 1; j++)
-            maxVal = max((long long)(maxLeft[j - 1] - nums[j]) * maxRight[j + 1], maxVal);
+            maxVal = max((long long)(maxLeft[j] - nums[j]) * maxRight[j], maxVal);
         
         return maxVal;
 
