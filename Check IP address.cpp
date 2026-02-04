7081f10f19fc7b6bda80f850993e63d845160959
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2026-02-04 14:50:30 +0530

    updated

--- Check IP address.cpp
+++ Check IP address.cpp
@@ -1,71 +1,69 @@
-// Online C++ compiler to run C++ program online
-// The IP address must be in the form of string like A.B.C.D
-// The length of A, B, C, or D can't be greater than 3.
-// The value of A, B, C, and D should lie between 0 to 255. Following are the valid
-// combinaons of each number of an IP address.
-// Any one- or two-digit number.
-// Any 3-digit number beginning with 1.
-// Any 3-digit number beginning with 2 if the second digit is 0 through 4.
-// Any 3-digit number beginning with 25 if the third digit is 0 through 5.
-// Leading 0's is not allowed.
-// Example
-// Input: str = "172.16.254.1"
-// Output: True
-// Input: str = "000.123.12.23.28"
-// Output: False
-// Input: str = "I.Am.not.an.ip"
+// Check IP address: 
+// The IP address must be in the form of string like A.B.C.D 
+// The length of A, B, C, or D can't be greater than 3. 
+// The value of A, B, C, and D should lie between 0 to 255. Following are the valid 
+// combina ons of each number of an IP address. 
+// Any one- or two-digit number. 
+// Any 3-digit number beginning with 1. 
+// Any 3-digit number beginning with 2 if the second digit is 0 through 4. 
+// Any 3-digit number beginning with 25 if the third digit is 0 through 5. 
+// Leading 0's is not allowed. 
+// Example 
+// Input: str = "172.16.254.1" 
+// Output: True 
+// Input: str = "000.123.12.23.28" 
 // Output: False 
-  
-#include <iostream>
-#include <string>
+// Input: str = "I.Am.not.an.ip" 
+// Output: False
 
+#include<iostream>
 using namespace std;
 
-int main() {
-    
-    string ip;
-    char ch = '.';
-    cout<<"Enter IP address : "
-    cin >> ip;
+bool legitip(int temp)
+{
+    if(temp >= 1 and temp <= 255)
+        return 1;
+    else
+        return 0;
+}
 
-    string strno="";
-    int flag=1;
-    int count=0;
-    
-    for(int i=0; i<ip.length()+1; i++)
+int main()
+{
+    // string ip = "172.001.255.1." ;
+    string ip = "172.16.254.1" ;
+    // cin >> ip;
+
+    int length = ip.length();
+    char no;
+    int temp = 0;
+    int count = 0;
+    bool flag = 1;
+
+    for(int i = 0; i < length; i++)
     {
-        if(ip[i]==ch || ip[i]=='\0')
-        {
-            if(strno.size()>1 and strno[0]=='0')
+        if(ip[i] - 48 >= 0 and ip[i] - 48 <= 9)
+            temp = temp * 10 + (ip[i] - 48);
+        else
+            if(ip[i] == '.' and count < 3)
             {
-                flag=0;
-                break;
+                if(!legitip(temp))
+                    flag = 0;
+
+                count++;
+                temp = 0;
             }
             else
             {
-                int myint1 = stoi(strno);
-                if(myint1 >=0 && myint1 <= 255)
-                {
-                    count++;
-                }
-                else
-                {
-                    flag=0;
-                    break;
-                }
-                strno="";
-            }
-        }
-        else
-        {
-            strno=strno+ip[i];
-        }
-        
+                flag = 0;
+                break;
+            }          
     }
-    
-    if(flag==1 and count==4)
-        cout<<"True";
+
+    if(!legitip(temp))
+        flag = 0;
+
+    if(flag and count == 3)
+        cout << "legit ip : " <<  ip;
     else
-        cout<<"False";
-    return 0;
+        cout << "false ip :" << ip;
 }
