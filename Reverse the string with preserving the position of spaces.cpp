3c2be31e11ffe51e854f701fe9e705de908791c9
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2026-02-05 17:30:38 +0530

    Update Reverse the string with preserving the position of spaces.cpp

--- Reverse the string with preserving the position of spaces.cpp
+++ Reverse the string with preserving the position of spaces.cpp
@@ -1,48 +1,33 @@
-// Online C++ compiler to run C++ program online
-// Reverse the string with preserving the position of spaces :
-// Write a Java program to reverse a string with preserving the position of spaces.
-// For example, if “I Am Not String” is the given string then the reverse of this
-// string with preserving the position of spaces is “g ni rtS toNmAI”.
-// Output :
-// I Am Not String —> g ni rtS toNmAI
-// JAVA JSP ANDROID —> DIOR DNA PSJAVAJ
+// Reverse the string with preserving the position of spaces : 
+// Write a Java program to reverse a string with preserving the position of spaces. 
+// For example, if “I Am Not String” is the given string then the reverse of this 
+// string with preserving the position of spaces is “g ni rtS toNmAI”. 
+// Output : 
+// I Am Not String —> g ni rtS toNmAI 
+// JAVA JSP ANDROID —> DIOR DNA PSJAVAJ 
 // 1 22 333 4444 55555 —> 5 55 554 4443 33221 
 
 #include <iostream>
-#include <string>
 
 using namespace std;
-int main() 
-{
-    string s; 
-    cout<<"Enter a string with white spaces : ";
-    getline(cin,s); // Example : "I Am Not String"
-    
-    string reversed_string_without_space="";
 
-    int array[s.length()]={};
-    int array_index=0;
+int main()
+{
+    string s = "I Am Not String"; 
+    string rev = "";
+    int index = 0;
+
+    for(int i = s.size() - 1; i >= 0; i--)
+        if(s[i] != ' ')
+            rev += s[i];
     
-    for(int i=0; i<s.length();i++)
-        if(s[i]==' ')
-            array[array_index++]=i;  // 1480000000000
-            
-    for(int i=s.length()-1; i>=0;i--)
-        if(s[i]!=' ')
-            reversed_string_without_space=reversed_string_without_space+s[i]; //gnirtStoNmAI
-            
-    array_index=0; 
-    int string_index=0;
-    for(int i=0; i<s.length(); i++) // Iterate or Traverse through the string length number of times.
+    for(int i = 0; i < s.size(); i++)
     {
-        if(array[array_index]==i)
-        {
-            cout<<" ";  //Print space on the particular index of the string.
-            array_index++; //Update index of the array
-        }
+        if(s[i] == ' ')
+            cout << " ";
         else
-        cout<<reversed_string_without_space[string_index++]; //Print the reversed string and update the string index after everytime it gets print or the else part gets executed.
+            cout << rev[index++];
     }
-    
+
     return 0;
 }
