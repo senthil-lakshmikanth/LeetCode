b00919a97bfd849c46e25ff6a7376fbddf8b46c6
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2026-02-05 17:28:23 +0530

    Update Harshad Number (Niven Number) Program.cpp

--- Harshad Number (Niven Number) Program.cpp
+++ Harshad Number (Niven Number) Program.cpp
@@ -1,45 +1,42 @@
-// Online C++ compiler to run C++ program online
-// Harshad number or Niven number is a number which is divisible by the sum of
-// its digits. For example, 21 is a Harshad number because 21 is divisible by the
-// sum of its digits (2+1=3). An interviewer ask this type of logical programs in
-// interview to test how do you implement logic in your code.
-// What Is Harshad Number Or Niven Number?
-// Harshad number or Niven number is a number which is divisible by the sum of
-// its digits.
-// For example,
-// 21 is a Harshad number because it is divisible by the sum of its digits.
-// 21 –> sum of digits –> 2+1 = 3 and 21 is divisible by 3 –> 21/3 = 7.
-// 111 is a Harshad number because it is divisible by the sum of its digits.
-// 111 –> sum of digits –> 1+1+1 = 3 and 111 is divisible by 3 –> 111/3 = 37
-// 153 is a Harshad number. It is divisible by the sum of its digits.
-// 153 –> sum of its digits –> 1+5+3 = 9 and 153 is divisible by 9 –> 153/9 = 17
+// Harshad Number (Niven Number) Program : 
+// Harshad number or niven number is a number which is divisible by the sum of 
+// its digits. For example, 21 is a Harshad number because 21 is divisible by the 
+// sum of its digits (2+1=3). An interviewer ask this type of logical programs in 
+// interview to test how do you implement logic in your code. 
+// What Is Harshad Number Or Niven Number? 
+// Harshad number or Niven number is a number which is divisible by the sum of 
+// its digits.  
+// For example, 
+// 21 is a Harshad number because it is divisible by the sum of its digits. 
+// 21 –> sum of digits –> 2+1 = 3 and 21 is divisible by 3 –> 21/3 = 7. 
+// 111 is a Harshad number because it is divisible by the sum of its digits. 
+// 111 –> sum of digits –> 1+1+1 = 3 and 111 is divisible by 3 –> 111/3 = 37 
+// 153 is a Harshad number. It is divisible by the sum of its digits. 
+// 153 –> sum of its digits –> 1+5+3 = 9 and 153 is divisible by 9 –> 153/9 = 17 
 
 #include <iostream>
 
 using namespace std;
 
-int main() 
+int SumOfDigits(int n)
 {
-    int n;
-    cout<<"Enter a number : ";
-    cin>>n;
-    
-    int reminder;
-    int sum=0;
-    
-    int test=n;
-    
-    while(test!=0)
+    int sum = 0;
+    while(n)
     {
-        reminder=test%10;
-        sum=sum+reminder;
-        test=test/10;
+        sum += n % 10;
+        n /= 10;
     }
-    
-    if(n%sum==0)
-        cout<<n<<" is a Harshad number because it is divisible by the sum of its digits.";
-    else
-        cout<<n<<" is not a Harshad / Niven Number !";
+    return sum;
+}
 
+int main()
+{
+    int n = 154;
+
+    if(n % SumOfDigits(n))
+        cout << "No";
+    else
+        cout << "Yes"; 
+    
     return 0;
 }
